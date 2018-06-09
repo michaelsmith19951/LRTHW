5554f2b9c25bf89f4d291fbd5469c966fa0b3ed6
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-08 20:43:35 -0400

    Second exercise showing the use of comments

---
+++ exercise2/ex2.rb
@@ -0,0 +1,8 @@
+#Comments are ignored by Ruby.
+#Comments are simply notes directed towards programmers that explain how something works, or to 'disable' something that doesn't work.
+
+puts "I could have code like this" #and the rest is ignored.
+
+#puts "This won't run."
+
+puts "This will run."
