62c3af2324dbbaf4e57e92b0f4d187db3ceb3a79
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-11 15:39:26 -0400

    Updated .rb file

--- exercise5/ex5.rb
+++ exercise5/ex5.rb
@@ -6,13 +6,17 @@ eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
-
+#Replace "name" with "Michael B Smith"
 puts "Let's talk about #{name}."
+#Replace "height" with "74"
 puts "He's #{height} inches tall."
+#Replace "weight" with "155"
 puts "He's #{weight} pounds heavy."
 puts "Actually that's not too heavy."
+#Replace "eyes" with "blue" and replace "hair" with "brown"
 puts "He's got #{eyes} eyes and #{hair} hair."
+#Replace "teeth" with "white"
 puts "His teeth are usually #{teeth} depending on the coffee."
 
-
+#Add "age", "height" and "weight" values up to get a total
 puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
