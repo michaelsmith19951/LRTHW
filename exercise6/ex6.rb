a14c0ad1ec581eb75ad9d9c66cee9d815c7b82e1
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-11 16:51:37 -0400

    Sixth exercise that combines strings and variables

---
+++ exercise6/ex6.rb
@@ -0,0 +1,29 @@
+#The types of people is equal to 10
+types_of_people = 10
+#Incorporating a variable into a string
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+#Print x and y variables onto the screen
+puts x
+puts y
+
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+#Boolean value that is false
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#Prints variable with boolean value
+puts joke_evaluation
+
+#Incorporating 2 variables into a string
+w = "This is the left side of..."
+e = "a string with a right side."
+
+#Prints both variables to the screen after combining (adding) them together
+puts w + e
