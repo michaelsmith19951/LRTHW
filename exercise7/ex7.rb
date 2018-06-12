259b957f46cc7fa31df0981d24d78f745b93093f
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-11 21:19:11 -0400

    Seventh exercise that prints text to console using variables

---
+++ exercise7/ex7.rb
@@ -0,0 +1,22 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+puts "." * 10 # Multiply the "." by 10, so it creates 10 periods
+
+# Spell the word "CheeseBurger" by assigning each letter in the word to a variable
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Print the word "CheeseBurger" to the console
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
