67c5db5d8bff3b740fefafb7701fee2565fb58ef
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-13 11:23:27 -0400

    Eleventh exercise that asks questions to be answered by user

---
+++ exercise11/ex11.rb
@@ -0,0 +1,15 @@
+# Print question to the console
+print "How old are you? "
+# Ask the user to input the "age" 
+age = gets.chomp
+# Print question to the console
+print "How tall are you? "
+# Ask the user to input the "height"
+height = gets.chomp
+# Print question to the console
+print "How much do you weigh? "
+# Ask the user to input the "weight"
+weight = gets.chomp
+
+# Print a summary of what the user inputted to the console
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
