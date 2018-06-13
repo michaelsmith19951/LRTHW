fa3ffb9f5f3bb09d68237ea24abd804b71f340ed
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-13 09:47:09 -0400

    Ninth exercise that prints text to screen on its ow line

---
+++ exercise9/ex9.rb
@@ -0,0 +1,17 @@
+# Set a variable called "days" equal to the days of the week
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Set a variable called "months" equal to the months of the year, with the "n" value in front of each month. The "n" value places each month on its own line.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print the days of the week to the console
+puts "Here are the days: #{days}"
+# Print the months of the week to the console
+puts "Here are the months: #{months}"
+
+# Print these 4 lines of text to the console
+puts %q{
+	There's something going on here.
+	With this weird quote
+	We'll be able to type as much as we like.
+	Even 4 lines if we want, or 5, or 6.
+}
