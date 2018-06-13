27c6ac6ce6b6653b126702cb89aa66f35e1f314c
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-13 10:51:17 -0400

    Added comments in .rb file

--- exercise10/ex10.rb
+++ exercise10/ex10.rb
@@ -1,15 +1,21 @@
+# Set the variable called "tabby_cat" equal to a line of text that is indented
 tabby_cat = "\tI'm tabbed in."
+# Set the variable called "persian_cat" equal to a line of text that is placed onto two lines
 persian_cat = "I'm split\non a line."
+# Set the variable called "backslash_cat" equal to a line of text that converts two \\ to one \
 backslash_cat = "I'm \\ a \\ cat."
 
-
+# Set the variable called "fat_cat" equal to an empty string.
 fat_cat = """
+
 I'll do a list:
-\t* Cat food
+\t* Cat food 
 \t* Fishies
 \t* Catnip\n\t* Grass
 """
+#The 3 lines above are indented with \t
 
+#Print these 4 variables to the console
 puts tabby_cat
 puts persian_cat
 puts backslash_cat
