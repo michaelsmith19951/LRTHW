f58ce85891768f76998c8f08acc988ae8c3fc922
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-15 20:06:04 -0400

    Thirteenth exercise that uses ARGV for the first time

---
+++ exercise13/ex13.rb
@@ -0,0 +1,7 @@
+# Any three variables can be inputted by the user through ARGV
+first, second, third = ARGV
+
+#The three variables are printed to the screen
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
---
+++ exercise13/readme.txt
@@ -0,0 +1,5 @@
+This is the thirteenth exercise that uses the "ARGV" command. "ARGV" is "argument variable", and it holds arguments that the user gives when running the program through the command line. 
+
+Example:
+
+ruby ex13.rb first 2nd 3rd
