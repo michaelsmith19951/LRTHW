71a8c060e0dd19eeed9d4f46dd69c32911a334ae
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-18 16:09:49 -0400

    Completed code

--- exercise21/ex21.rb
+++ exercise21/ex21.rb
@@ -22,3 +22,16 @@ end
 
 puts "Let's do some math with just functions!"
 
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+
+puts "Here is a puzzle."
+
+what = add(age, subtract(height, multiply, (weight, divide(iq, 2))))
+
+puts "That becomes: #{what}. Can you do it by hand?"
