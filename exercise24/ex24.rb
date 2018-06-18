7d15cce975ffe4ea5a8123f8bad40d710e4c12cb
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-18 16:49:32 -0400

    Added poem

--- exercise24/ex24.rb
+++ exercise24/ex24.rb
@@ -1,1 +1,20 @@
 puts "Let's practice everything."
+puts 'You\ 'd need to know \ 'bout escapes with \\ that do \n newlines and \t tabs.'
+
+
+poem = <<END
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+END
+
+puts "------------"
+puts poem
+puts "------------"
+
+
+five = 10 - 2 + 3 - 6
+puts "This should be five: #{five}"
